9d756d45db5beb89093d4f278bc911943557bcf8
o.mannion <o.mannion@ca920bae-ce15-ce9f-1173-02897e44c0fb>
2011-06-10 03:48:26 +0000

    v1.2.2: New StringUtil class
    git-svn-id: https://omcutil.googlecode.com/svn/trunk@34 ca920bae-ce15-ce9f-1173-02897e44c0fb

---
+++ omcutil/src/org/omancode/util/StringUtil.java
@@ -0,0 +1,46 @@
+package org.omancode.util;
+
+/**
+ * Static utility class of string related functions.
+ * 
+ * @author Oliver Mannion
+ * @version $Revision$
+ */
+public final class StringUtil {
+
+	private StringUtil() {
+		// static util class
+	}
+
+	/**
+	 * Construct a function call string, eg: "func(1,2,3)".
+	 * 
+	 * @param functionName
+	 *            function names
+	 * @param args
+	 *            string args
+	 * @return function call string
+	 */
+	public static String functionCall(String functionName, String... args) {
+		StringBuilder sb = new StringBuilder(64);
+
+		sb.append(functionName).append("(");
+		int i = 0;
+		for (; i < args.length - 1; i++) {
+			sb.append(args[i]).append(", ");
+		}
+		sb.append(args[i]).append(")");
+		return sb.toString();
+	}
+
+	/**
+	 * Wrap the string in double quotes.
+	 * 
+	 * @param str
+	 *            input string
+	 * @return s wrapped in double quotes.
+	 */
+	public static String doublequote(String str) {
+		return "\"" + str + "\"";
+	}
+}
