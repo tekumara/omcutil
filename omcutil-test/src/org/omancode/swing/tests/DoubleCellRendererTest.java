ff0fa6296bf36895b11a6ea3dbefceddb6b6b09b
o.mannion@gmail.com <o.mannion@gmail.com@ca920bae-ce15-ce9f-1173-02897e44c0fb>
2012-01-16 21:42:04 +0000

    DoubleCellRendererTest OSGIfied
    git-svn-id: https://omcutil.googlecode.com/svn/trunk@41 ca920bae-ce15-ce9f-1173-02897e44c0fb

--- omcutil-test/src/org/omancode/swing/tests/DoubleCellRendererTest.java
+++ omcutil-test/src/org/omancode/swing/tests/DoubleCellRendererTest.java
@@ -3,7 +3,6 @@ package org.omancode.swing.tests;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
 import org.junit.BeforeClass;
@@ -12,7 +11,8 @@ import org.omancode.util.swing.DoubleCellRenderer;
 
 public class DoubleCellRendererTest {
 
-	private static DoubleCellRenderer renderer;
+	private static DoubleCellRenderer renderer10;
+	private static DoubleCellRenderer renderer2;
 	private static final double dbl20digits = 1.12345678911234567890;
 	private static final double dbl5zeros = 0.0000012345;
 	
@@ -20,30 +20,34 @@ public class DoubleCellRendererTest {
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		renderer = new DoubleCellRenderer(10);
+		renderer10 = new DoubleCellRenderer(10);
+		renderer2 = new DoubleCellRenderer(2);
 	}
 
 	@Test
 	public void testFormatter() {
-		NumberFormat formatter = renderer.getFormatter();
-		
+		NumberFormat formatter = renderer10.getFormatter();
 		
 		
 		System.out.println("MaximumFractionDigits: " + formatter.getMaximumFractionDigits());
 		System.out.println("MinimumFractionDigits: " + formatter.getMinimumFractionDigits());
 		
-		renderer.setValue(dbl20digits);
-		System.out.println(renderer.getText());
-		assertThat(renderer.getText().length(), equalTo(12));
+		renderer10.setValue(dbl20digits);
+		System.out.println(renderer10.getText());
+		assertThat(renderer10.getText().length(), equalTo(12));
 		
-		renderer.setValue(dbl5zeros);
-		System.out.println(renderer.getText());
-		assertThat(renderer.getText().length(), equalTo(12));
+		renderer10.setValue(dbl5zeros);
+		System.out.println(renderer10.getText());
+		assertThat(renderer10.getText().length(), equalTo(12));
 		
 		//renderer = DecimalFormat.getInstance();
 		
-		renderer.setValue(1.12E-19);
-		System.out.println(renderer.getText());
+		renderer10.setValue(1.12E-19);
+		System.out.println(renderer10.getText());
+		
+		renderer2.setValue(1.304);
+		System.out.println(renderer2.getText());
+		assertThat(renderer2.getText().length(), equalTo(4));
 		
 	}
 
